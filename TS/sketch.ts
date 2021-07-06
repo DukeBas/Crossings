c1dd157776dec60ba26c471f4b6e2a8f0e5d0389
DukeBas <35384117+DukeBas@users.noreply.github.com>
2021-07-06 18:45:32 +0200

    Working no intersections

--- TS/sketch.ts
+++ TS/sketch.ts
@@ -1,5 +1,12 @@
-// Global variables and types
-//...
+const maxTries = 30; // maximum amount of times a line can be regenerated per loop
+
+type Line = {
+  start: p5.Vector,
+  end: p5.Vector,
+}
+
+// Array holding all the previously placed lines
+const lines: Line[] = [];
 
 // run before first drawn frame
 function setup() {
@@ -7,6 +14,9 @@ function setup() {
   canvas.position(0, 0);  // make canvas start in top-left corner
   canvas.style('z-index', '-1');  // set canvas as background
   frameRate(60);  // target framerate
+
+  stroke(255);  // set drawing color to white
+  strokeWeight(0.5);
 }
 
 // automatically called function to make canvas resize with window
@@ -16,5 +26,52 @@ function windowResized() {
 
 // single drawing iteration
 function draw() {
+  // get a new line that doesn't intersect
+  let newLine: Line;
+  let attemptNumber = 0;
+  do {
+    attemptNumber++;
+    newLine = {
+      start: getPoint(),
+      end: getPoint(),
+    }
+  } while (testLineAgainstSet(newLine, lines, IntersectionTest) && attemptNumber < maxTries);
 
+  // only do things if we found a new valid line
+  if (attemptNumber < maxTries){
+    // add the line to the known lines
+    lines.push(newLine);
+
+    // draw the line
+    line(newLine.start.x, newLine.start.y, newLine.end.x, newLine.end.y);
+  }
+
+}
+
+// generates a random point on the canvas, returned as vector
+function getPoint(): p5.Vector {
+  return createVector(random(0, width), random(0, height));
+}
+
+// tests whether two linesegments intersect using determinant and testing if intersection is in the segments
+// returns true if lines intersect, else false
+// based on https://stackoverflow.com/a/24392281
+function IntersectionTest(A: Line, B: Line): boolean {
+  const det = (A.end.x - A.start.x) * (B.end.y - B.start.y) - (B.end.x - B.start.x) * (A.end.y - A.start.y);
+  if (det === 0) {
+    // parallel lines
+    return false;
+  } else {
+    // check if the intersection is in the segments
+    const lambda = ((B.end.y - B.start.y) * (B.end.x - A.start.x) + (B.start.x - B.end.x) * (B.end.y - A.start.y)) / det;
+    const gamma = ((A.start.y - A.end.y) * (B.end.x - A.start.x) + (A.end.x - A.start.x) * (B.end.y - A.start.y)) / det;
+    return (0 < lambda && lambda < 1) && (0 < gamma && gamma < 1);
+  }
+}
+
+// tests a line against a set of lines using some set of lines using a given function
+function testLineAgainstSet(A: Line, Set: Line[], func: Function): boolean {
+  return Set.some((line) => {
+    return func(A, line);
+  });
 }
--- build/build.js
+++ build/build.js
@@ -4,15 +4,51 @@ function openSidebar() {
 function closeSidebar() {
     document.getElementById("sidebar").style.width = "0";
 }
+var maxTries = 30;
+var lines = [];
 function setup() {
     var canvas = createCanvas(windowWidth, windowHeight);
     canvas.position(0, 0);
     canvas.style('z-index', '-1');
     frameRate(60);
+    stroke(255);
+    strokeWeight(0.5);
 }
 function windowResized() {
     resizeCanvas(windowWidth, windowHeight);
 }
 function draw() {
+    var newLine;
+    var attemptNumber = 0;
+    do {
+        attemptNumber++;
+        newLine = {
+            start: getPoint(),
+            end: getPoint(),
+        };
+    } while (testLineAgainstSet(newLine, lines, IntersectionTest) && attemptNumber < maxTries);
+    if (attemptNumber < maxTries) {
+        lines.push(newLine);
+        line(newLine.start.x, newLine.start.y, newLine.end.x, newLine.end.y);
+    }
+}
+function getPoint() {
+    return createVector(random(0, width), random(0, height));
+}
+function IntersectionTest(A, B) {
+    var det = (A.end.x - A.start.x) * (B.end.y - B.start.y) - (B.end.x - B.start.x) * (A.end.y - A.start.y);
+    if (det === 0) {
+        return false;
+    }
+    else {
+        var lambda = ((B.end.y - B.start.y) * (B.end.x - A.start.x) + (B.start.x - B.end.x) * (B.end.y - A.start.y)) / det;
+        var gamma = ((A.start.y - A.end.y) * (B.end.x - A.start.x) + (A.end.x - A.start.x) * (B.end.y - A.start.y)) / det;
+        return (0 < lambda && lambda < 1) && (0 < gamma && gamma < 1);
+    }
+}
+function testLineAgainstSet(A, Set, func) {
+    return Set.some(function (line) {
+        return func(A, line);
+    });
 }
 //# sourceMappingURL=../TS/TS/build.js.map
